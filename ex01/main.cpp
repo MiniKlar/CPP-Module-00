f8515c54b3192e406eb816ee7934608fa0137148
MiniKlar <omontloris@gmail.com>
2025-09-24 03:47:28 +0200

    - Refactored main function to improve command handling and error messaging; enhance user prompts for better interaction.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,35 +6,43 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 21:05:29 by lomont            #+#    #+#             */
-/*   Updated: 2025/09/23 20:36:29 by lomont           ###   ########.fr       */
+/*   Updated: 2025/09/24 02:23:44 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Phonebook.hpp"
-#include <iostream>
 
 int main(void){
 	Phonebook book;
 	std::string command;
+	std::string errorMessage = "";
 
-	std::cout << "Please enter a command: " << std::endl << "- ADD" << std::endl << "- SEARCH" << std::endl << "- EXIT" << std::endl;
 	while (1) {
+		printLogo();
+		if (!errorMessage.empty())
+		{
+			std::cout << "!! " << errorMessage << " !!" << std::endl << std::endl;
+			errorMessage.clear();
+		}
+		std::cout << "Please enter a command (ADD, SEARCH or EXIT) " << ": ";
 		std::getline(std::cin, command);
 		if (command.compare("ADD") == 0) {
 			book.FillContact();
 		}
-		else if (command.compare("SEARCH") == 0)
-			book.display_contact(book.array);
+		else if (command.compare("SEARCH") == 0) {
+			if (book.count > 0)
+				book.display_contact(book.array);
+			else {
+				clearTerminal();
+				errorMessage = "You can't search into a Phonebook empty, please ADD a contact!";
+			}
+		}
 		else if (command.compare("EXIT") == 0)
 			return 0;
 		else {
-			std::cout << "Command invalid. Please used 'ADD', 'SEARCH, or 'EXIT' to navigate the Phonebook" << std::endl;
+			clearTerminal();
+			errorMessage = "Command invalid. Please used 'ADD', 'SEARCH, or 'EXIT' to navigate the Phonebook";
 		}
 	}
-	// std::cout << book.array[0].firstName << std::endl;
-	// std::cout << book.array[0].lastName << std::endl;
-	// std::cout << book.array[0].Nickname << std::endl;
-	// std::cout << book.array[0].phoneNumber << std::endl;
-	// std::cout << book.array[0].darkestSecret << std::endl;
 	return (0);
 }
