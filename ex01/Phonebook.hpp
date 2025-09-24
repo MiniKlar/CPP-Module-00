6c6b32ccc109dc47c1d1f87d34f6b5fcbf06d1e9
MiniKlar <omontloris@gmail.com>
2025-09-24 20:52:25 +0200

    - Refactored Phonebook and Contact classes: improved access control by moving member variables to private, updated method declarations for consistency, and removed redundant method signatures.

--- ex01/Phonebook.hpp
+++ ex01/Phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 20:45:49 by lomont            #+#    #+#             */
-/*   Updated: 2025/09/24 03:42:57 by lomont           ###   ########.fr       */
+/*   Updated: 2025/09/24 20:25:25 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,34 +19,53 @@
 #include "UI.hpp"
 
 class Contact {
-	public:
+	private:
 		std::string firstName;
 		std::string lastName;
 		std::string Nickname;
 		std::string phoneNumber;
 		std::string darkestSecret;
-
+	public:
 		Contact( void );
 		~Contact( void );
+
+		// Getters
+
+		std::string getFirstName() const;
+		std::string getLastName() const;
+		std::string getNickname() const;
+		std::string getPhoneNumber() const;
+		std::string getDarkestSecret() const;
+		// Setters
+
+		void setFirstName(const std::string& name);
+		void setLastName(const std::string& name);
+		void setNickname(const std::string& nickname);
+		void setPhoneNumber(const std::string& phone);
+		void setDarkestSecret(const std::string& secret);
 };
 
 class Phonebook {
-	public:
-		Contact array[8];
+	private:
 		int count;
+		Contact array[8];
+
+		void fill_contact_field(const int i, int contact_index, const std::string& tmp);
+		void check_if_valid_input(std::string& str, int i);
+		bool check_phone_number(const std::string& str);
+		bool check_name(const std::string& str);
+		bool check_nickname(const std::string& str);
+		bool check_secret(const std::string& str);
+		int getContactsIndex(const Contact *contact);
+		void printContacts(const Contact *contact, int maxWidth, int index);
+
+	public:
 		Phonebook( void );
 		~Phonebook( void );
+		int getCount() const;
+		const Contact *getContacts() const;
 		void FillContact( void );
-		void print_input_message (int i);
-		void fill_contact_field(int i, int contact_index, std::string tmp);
-		void check_if_valid_input(std::string *str, int i);
-		bool check_phone_number(std::string str);
-		bool check_name(std::string str);
-		bool check_nickname(std::string str);
-		bool check_secret(std::string str);
-		void display_contact(Contact *contact);
-		int check_contacts(Contact *contact);
-		void printContacts(Contact *contact, int maxWidth, int index);
+		void display_contact(const Contact *contact);
 };
 
 #endif
