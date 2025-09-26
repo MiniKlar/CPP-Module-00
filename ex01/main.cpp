40f30b7d18ecc3f18cb2611c4357b70c0c327b2d
MiniKlar <omontloris@gmail.com>
2025-09-26 19:02:55 +0200

    - Refactored error handling and function calls for consistency in main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 21:05:29 by lomont            #+#    #+#             */
-/*   Updated: 2025/09/24 20:48:01 by lomont           ###   ########.fr       */
+/*   Updated: 2025/09/26 18:47:25 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,40 @@
 int main(void){
 	Phonebook book;
 	std::string command;
-	std::string errorMessage = "";
+	std::string ErrorMessage = "";
 
 	while (1) {
-		printLogo();
-		if (!errorMessage.empty())
+		PrintLogo();
+		if (!ErrorMessage.empty())
 		{
-			std::cout << "!! " << errorMessage << " !!" << std::endl << std::endl;
-			errorMessage.clear();
+			std::cout << "!! " << ErrorMessage << " !!" << std::endl << std::endl;
+			ErrorMessage.clear();
 		}
 		std::cout << "Please enter a command (ADD, SEARCH or EXIT) " << ": ";
 		if (!std::getline(std::cin, command)) {
-			clearTerminal();
+			ClearTerminal();
 			break;
 		}
 		else if (command.empty()) {
-			clearTerminal();
+			ClearTerminal();
 			continue;
 		}
 		else if (command.compare("ADD") == 0) {
 			book.FillContact();
 		}
 		else if (command.compare("SEARCH") == 0) {
-			if (book.getCount() > 0)
-				book.display_contact(book.getContacts());
+			if (book.GetCount() > 0)
+				book.DisplayContact(book.GetContacts());
 			else {
-				clearTerminal();
-				errorMessage = "You can't search into a Phonebook empty, please ADD a contact!";
+				ClearTerminal();
+				ErrorMessage = "You can't search into a Phonebook empty, please ADD a contact!";
 			}
 		}
 		else if (command.compare("EXIT") == 0)
 			return 0;
 		else {
-			clearTerminal();
-			errorMessage = "Command invalid. Please used 'ADD', 'SEARCH, or 'EXIT' to navigate the Phonebook";
+			ClearTerminal();
+			ErrorMessage = "Command invalid. Please used 'ADD', 'SEARCH, or 'EXIT' to navigate the Phonebook";
 		}
 	}
 	return (0);
